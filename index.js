2d036c0852d5686a392283e50cc06d28d3bffc19
Matthew Francis Brunetti <zenflow87@gmail.com>
2020-08-16 16:46:19 -0400

    Use same major version of all dependencies as tsdx

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ module.exports = {
   extends: [
     require.resolve("eslint-config-react-app"),
     require.resolve("eslint-config-prettier/@typescript-eslint"),
-    "plugin:prettier/recommended",
+    "plugin:prettier/recommended"
   ],
   settings: {
     react: {
-      version: "999.999.999",
-    },
-  },
+      version: "999.999.999"
+    }
+  }
 };
